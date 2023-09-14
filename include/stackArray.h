b26970d8464d33cfb295a07bd480e8c9bfd74164
Riel <jasper.apos@gmail.com>
2023-09-14 22:04:58 +0800

    typedef struct stack

--- include/stackArray.h
+++ include/stackArray.h
@@ -5,7 +5,7 @@
 #include <stdio.h>
 #define MAX 5
 
-typedef struct {
+typedef struct Stack{
     char elem[MAX];
     int top;
 } Stack;
--- src/main.c
+++ src/main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "../include/stackLinked.h"
+#include "../include/stackArray.h"
 
 int main(int argc, char const *argv[])
 {
@@ -12,14 +12,14 @@ int main(int argc, char const *argv[])
     push(&myStack, 'C');
     push(&myStack, 'S');
     push(&myStack, 'U');
-    pop(&myStack);
-    pop(&myStack);
-    pop(&myStack);
-    pop(&myStack);
-    pop(&myStack);
-    pop(&myStack);
-    pop(&myStack);
-    pop(&myStack);
+    // pop(&myStack);
+    // pop(&myStack);
+    // pop(&myStack);
+    // pop(&myStack);
+    // pop(&myStack);
+    // pop(&myStack);
+    // pop(&myStack);
+    // pop(&myStack);
     display(myStack);
 
     return 0;
